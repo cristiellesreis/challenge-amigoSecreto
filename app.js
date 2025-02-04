63c75a510abfa5b19d23a176fe53c6218efb2b15
cristiellesreis <cristielle.santos94@gmail.com>
2025-02-04 17:22:06 -0300

    feat: função sortear amigo e refatoração

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 let listaAmigos = [];
 
 function adicionarAmigo() {
-  let nomeAmigo = document.querySelector('#amigo').value;
+  let nomeAmigo = document.querySelector('input').value.trim();
   
-  if (nomeAmigo) {
-    listaAmigos.push(nomeAmigo);
-    atualizarLista();
-    document.querySelector('#amigo').value = '';
-
-  } else if (nomeAmigo.trim() === ''){
+  if (nomeAmigo === '') {
     alert('Por favor, insira um nome.');
     return false;
-  } 
-  return true;
-}
+  }
 
+  listaAmigos.push(nomeAmigo);
+  atualizarLista();
+  document.querySelector('input').value = '';
+}
 
 function atualizarLista() {
-  let lista = document.querySelector('#listaAmigos');
-
+  let lista = document.querySelector('.name-list');
   lista.innerHTML = '';
 
-  listaAmigos.forEach((amigo) => {
+  for (let i = 0; i < listaAmigos.length; i++) {
     let item = document.createElement('li');
-
-    item.textContent = amigo;
+    item.textContent = listaAmigos[i];
     lista.appendChild(item);
-  });
+  }
 }
 
+function sortearAmigo() {
+  if (listaAmigos.length === 0) {
+    alert('Lista vazia! Adicione seus amigos!');
+    return;
+  }
 
-
-
-
+  let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+  let amigoSorteado = listaAmigos[indiceAleatorio];
+  
+  document.getElementById('resultado').innerHTML = 'O amigo sorteado é: ' + amigoSorteado;
+}
