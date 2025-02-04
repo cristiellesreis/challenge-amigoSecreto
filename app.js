f7a9c5b1d8b1f4aef07499d35f27f1b232e32b03
cristiellesreis <cristielle.santos94@gmail.com>
2025-02-04 16:00:14 -0300

    feat: função agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+  let nomeAmigo = document.querySelector('#amigo').value;
+  
+  if (nomeAmigo) {
+    listaAmigos.push(nomeAmigo);
+    atualizarLista();
+    document.querySelector('#amigo').value = '';
+
+  } else if (nomeAmigo.trim() === ''){
+    alert('Por favor, insira um nome.');
+    return false;
+  } 
+  return true;
+}
+
+
+function atualizarLista() {
+  let lista = document.querySelector('#listaAmigos');
+
+  lista.innerHTML = '';
+
+  listaAmigos.forEach((amigo) => {
+    let item = document.createElement('li');
+
+    item.textContent = amigo;
+    lista.appendChild(item);
+  });
+}
+
+
+
+
+
